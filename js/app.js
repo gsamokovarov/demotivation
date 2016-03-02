26d789ee9730ee8642fb79a6556da8781fe1096e
Genadi Samokovarov <gsamokovarov@gmail.com>
2016-03-02 19:21:30 +0200

    Ensure that the index will always be 0 or 1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var between = function(value, min, max) {
+    return value < min ? min : (value > max ? max : value);
+  };
+
   var images = ["regular_troll.png", "broken_troll.png"];
-  var randomImageIndex = Math.floor((Math.random() * 2) - 0.01);
+  var randomImageIndex = between(Math.floor(Math.random() * 2), 0, 1);
 
   var troll = document.getElementById('troll');
   troll.src = "images/" + images[randomImageIndex];
